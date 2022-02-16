38511f8d7e656e932ae8d65176dde8562af5fb40
Josh Yee <joshyee@gmail.com>
2022-02-15 16:12:58 -0800

    added mobile styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import "./styles.css"
 
-
 const navContainer = document.querySelector(".nav-container")
 
 navContainer.addEventListener("click", () => {
-    navContainer.classList.toggle("change")
-
-
+    toggleProjects()
 })
+
+  function toggleProjects() {
+    const allTasks = document.querySelector(".all-tasks")
+
+    if (allTasks.style.visibility == "visible") { 
+        allTasks.style.visibility = "hidden"
+        allTasks.style.opacity = "0"
+        allTasks.style.transition = "200ms"
+    } else {
+        allTasks.style.visibility = "visible"
+        allTasks.style.transition = "200ms"
+        allTasks.style.opacity = "1"
+
+    }
+  }
+
 
